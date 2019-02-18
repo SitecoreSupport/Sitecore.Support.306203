79dc8d8119bc617c4a709567c6236cd90410879c
Andrey Filchenkov <22079019+AndreyFilchenkov@users.noreply.github.com>
2019-02-18 10:04:18 +0200

    Fix issues: 306203, 311209, implement: 306142

---
+++ src/Sitecore.Support.306203/sitecore/shell/client/Applications/Publishing/PublishingDialog.js
@@ -0,0 +1,32 @@
+﻿(function (Speak) {
+
+    Speak.pageCode(["publishingUtils", "publishingConstants", "publishingShared"], function (Utils, Constants, Shared) {
+        var id;
+
+        return {
+            initialized: function () {
+
+                // Sitecore.Support.306142 +++
+                if (this.ButtonPublish)
+                {
+                    this.ButtonPublish.IsEnabled = false;
+                }
+                // Sitecore.Support.306142 ---
+
+                if (this.TabControlPublish.Items.length === 1) {
+                    $(".sc-tab-control-nav-wrap").hide();
+                }
+
+                id = Speak.utils.url.parameterByName(Constants.parameters.id);
+
+                if (id.length === 0) {
+                    this.HeaderTitleSitePublish.IsVisible = true;
+                    this.HeaderTitleItemPublish.IsVisible = false;
+                }
+
+                Shared.statusCheck.call(this);
+            }
+        }
+    });
+
+})(Sitecore.Speak);
---
+++ src/Sitecore.Support.306203/sitecore/shell/client/Applications/Publishing/PublishingPreferences.js
@@ -0,0 +1,45 @@
+﻿define(["publishingPreferences"],
+    function (Preferences) {
+        return {
+            SetCookie: function (subItems, relatedItems, languages, targets) {
+                var now = new Date();
+                now.setYear(now.getFullYear() + 1);
+
+                var languageList = languages.map(function (elem) {
+                    return elem.Code;
+                }).join(",");
+
+                var targetList = targets.map(function (elem) {
+                    return elem.Id;
+                }).join(",");
+
+                // Sitecore.Support.311209 +++ Prevent storing the cookie with the empty 'lang' or 'target' lists
+                if (languageList && targetList)
+                {
+                    var cookie = ["publishingpreferences", '=', 'subitems:' + subItems + '|related:' + relatedItems + '|lang:' + languageList + '|targets:' + targetList + '', '; path=/; expires=' + now.toUTCString() + ' ;'].join('');
+                    document.cookie = cookie;
+                }
+                // Sitecore.Support.311209 ---
+            },
+
+            LoadCookie: function () {
+                var result = document.cookie.match(new RegExp("publishingpreferences" + '=([^;]+)'));
+
+                if (result) {
+                    var preferences = result[1].split("|");
+
+                    var obj = new Object();
+
+                    preferences.forEach(
+                        function (entry) {
+                            var keyValue = entry.split(':');
+                            obj[keyValue[0]] = keyValue[1];
+                        }
+                    );
+
+                    return obj;
+                }
+            },
+        }
+    }
+);
